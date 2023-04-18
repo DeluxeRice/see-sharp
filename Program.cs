8ab3f384e757f895c2e2ed88a02a9bdeaa3bfa3d
Bryce Wu <brycewu6@gmail.com>
2023-04-18 16:09:19 -0700

    Wrote basic main for Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramNS
+{
+    class Program
+    {
+        public static void main(string[] args)
+        {
+            Console.Write("Hello World!");
+        }
+    }
+}
